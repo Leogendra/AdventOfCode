6bfccc16f942c8cd3a79b5bde7bf21e2d08b8e26
Gatien <61588710+Leogendra@users.noreply.github.com>
2022-11-30 10:17:07 +0000

    Préparation au day 1

--- 2022/util.py
+++ 2022/util.py
@@ -39,23 +39,3 @@ def print_tab(T):
       print("\n ",end="")
     print(T[i],end="")
   print("]\n")
-
-
-
-
-
-print("********** Day 1 **********")
-#Ouverture du fichier input
-input = ouvre("day1/input.txt")
-
-#comptage des incrementations
-cpt = 0
-n = len(input)
-print("Nombre d'inputs :",n)
-for i in range(1, n-2):
-  som1 = input[i-1] + input[i] + input[i+1]
-  som2 = input[i] + input[i+1] + input[i+2]
-  if som1 < som2:
-    cpt += 1
-    
-print("Nombre d'incréments :",cpt)
