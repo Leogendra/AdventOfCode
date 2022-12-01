8d4fd0f32bc13433a5597417b6f91045698525d8
Gatien <61588710+Leogendra@users.noreply.github.com>
2022-12-01 11:51:15 +0000

    Day 1

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -1,1 +1,21 @@
-import util.py
+top1 = 0
+top2 = 0
+top3 = 0
+with open("2022/day1/input.txt", "r") as f:
+	elf = 0
+	for line in f.readlines():
+		if line != "\n":
+			elf += int(line)
+		else:
+			if elf > top1:
+				top3 = top2
+				top2 = top1
+				top1 = elf
+			elif elf > top2:
+				top3 = top2
+				top2 = elf
+			elif elf > top3:
+				top3 = elf
+			elf = 0
+
+print(top1+top2+top3)
---
+++ main.py
@@ -0,0 +1,3 @@
+import os
+day = 1
+os.system(f"python3 2022/day{day}/day{day}.py")
