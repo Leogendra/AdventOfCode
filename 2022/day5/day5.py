5bfb1402b6e362e1383c65cde0bc1c45add2afe6
Gatien <61588710+Leogendra@users.noreply.github.com>
2022-12-05 14:49:45 +0000

    Day 5 hit

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -1,33 +1,67 @@
-import string
-fichier = "input"
-jeu = [line.strip() for line in open(f"2022/day3/{fichier}.txt","r")]
-#print(jeu)
+def deplacer(p, n, deb, fin):
+	for _ in range(n):
+		if p[deb] != []:
+			bloc = p[deb].pop()
+			p[fin].append(bloc)
+	return p
 
-letters = string.ascii_letters
-cpt = 0
-for line in jeu:
-	compartment1 = line[len(line)//2:]
-	compartment2 = line[:len(line)//2]
-	for c in compartment1:
-		if c in compartment2:
-			cpt += 1 + letters.index(c)
-			break
 
-print("star 1 :",cpt)
+def deplacerBloc(p, n, deb, fin):
+	blocs = []
+	taille = min(len(p[deb]),n)
+	
+	for _ in range(taille):
+		blocs.append(p[deb].pop())
+	
+	blocs.reverse()
+	for bloc in blocs:
+		p[fin].append(bloc)
+		
+	return p
 
-cpt = 0
-group = 0
-group_compartment = []
-for line in jeu:
-	group += 1
-	group_compartment.append(line)
-	if group == 3:
-		group = 0
-		for c in group_compartment[0]:
-			if c in group_compartment[1]:
-				if c in group_compartment[2]:
-					cpt += 1 + letters.index(c)
-					group_compartment = []
-					break
 
-print("star 2 :",cpt)
+
+for fichier in ["test", "input"]:
+	input = [line for line in open(f"2022/day5/{fichier}.txt","r")]
+	jeu = []
+	instructions = []
+	jeu_fini = False
+	for line in input:
+		if line.strip() == "":
+			jeu_fini = True
+			continue
+		if jeu_fini:
+			instructions.append([x for x in line.split() if x.isnumeric()])
+		else:
+			jeu.append(line)
+
+	# préparation pour parser l'entrée
+	jeu.reverse()
+	nbPiles = int(jeu[0].strip()[-1])
+	piles = [[] for _ in range(nbPiles)]
+	
+	# Parsing de l'entrée
+	for i in range(1,len(jeu)):
+		for j in range(nbPiles):
+			lettre = jeu[i][1+4*j]
+			if lettre != " ":
+				piles[j].append(lettre)
+
+	piles1 = piles.copy()
+	# lecture des instructions
+	for n, dep, fin in instructions:
+		piles1 = deplacer(piles1, int(n), int(dep)-1, int(fin)-1)
+	
+	list_top1 = [pile[-1] for pile in piles1 if pile != []]
+	
+	print("Part 1 :","".join(list_top1))
+
+
+	piles2 = piles.copy()
+	# lecture des instructions
+	for n, dep, fin in instructions:
+		piles2 = deplacerBloc(piles1, int(n), int(dep)-1, int(fin)-1)
+
+	list_top2 = [pile[-1] for pile in piles2 if pile != []]
+	
+	print("Part 2 :","".join(list_top2))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
 from datetime import datetime
 day = datetime.now().day
+print(f"☆ DAY {day} ☆")
 os.system(f"python 2022/day{day}/day{day}.py")
