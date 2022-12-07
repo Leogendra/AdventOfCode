94c3277e52e9d0257eea7f55f6193fda720a3ecf
Gatien <61588710+Leogendra@users.noreply.github.com>
2022-12-07 19:49:42 +0000

    Day 7 struggled

--- 2022/day7/day7.py
+++ 2022/day7/day7.py
@@ -1,60 +1,75 @@
+#import lesProblèmes
+
 def tailleDossier(d):
     if isinstance(d, list):
         return sum([tailleDossier(file) for file in d])
     else:
         return d
+        
 
+def dossiersAvecMoinsDe(d,n):
+    cpt = 0
+    if isinstance(d, list):
+        taille = tailleDossier(d)
+        if taille <= n:
+            cpt += taille
+        for doss in d:
+            cpt += dossiersAvecMoinsDe(doss,n)
+    return cpt
+    
 
-for fichier in ["test"]:
+def dossierASupprimer(doss,n):
+    dossierMin = float('inf')
+    if isinstance(doss, list):
+        tailleDoss = tailleDossier(doss)
+        if tailleDoss >= n: 
+            dossierMin = tailleDoss
+        for d in doss:
+            taille = tailleDossier(d)
+            if isinstance(d,list) and (taille >= n) and (taille < dossierMin):
+                dossierMin = min(taille,dossierASupprimer(d,n))
+    return dossierMin
+
+# CODE
+for fichier in ["test", "input"]:
+    print(f"\nFICHIER {fichier}.txt")
     commandes = [line.strip() for line in open(f"2022/day7/{fichier}.txt","r")]
-    cpt1 = 0
-    arbre = []    # arborescence
-    courant = []  # savoir où on se trouve
-    dico = {}     # correspondance nom de dossier/indice dans l'arbre
-    cptDossiers = 0
-
+    arbre = []      # arborescence
+    courant = []    # savoir où on se trouve
+    dico = {}       # correspondance nom de dossier/indice dans l'arbre
+    cptDossiers = 0 # compter le nb de dossiers dans le dossier courant
+    
     commandes.pop(0)
     for line in commandes:          # construction de l'arborescence
         cmd = line.split()
-        # print("\ndico :",dico)
-        # print("courant :",courant)
-        # print("arbre :",arbre)
-        # print("cmd :",cmd)
-
+        
         if cmd[0] == "$":           # si c'est un commande
             if cmd[1] == "cd":      # au met à jour notre pointeur courant
                 if cmd[2] == "..":
                     courant.pop()
                 else:
-                    courant.append(dico[cmd[2]])
-
+                    courant.append(dico[str(courant)][cmd[2]])
             elif cmd[1] == "ls":
                 cptDossiers = 0
                 continue
-        
-        else:                       # si pas une commande mais une liste de dossiers/fichiers
-            cur = arbre             # pointeur vers le dossier courant
+               
+        else:                       
+            ptrCourant = arbre             # pointeur vers le dossier courant
             for ind in courant:
-                cur = cur[ind]
-
+                ptrCourant = ptrCourant[ind]
+    
             if cmd[0] == "dir":
-                cur.append([])      # append le dossier dans l'arbre
-                dico[cmd[1]] = cptDossiers
-            else:   
-                cur.append(int(cmd[0]))  # append le fichier dans l'arbre
-            
+                ptrCourant.append([])      # append le dossier dans l'arbre
+                if str(courant) in dico:
+                    dico[str(courant)][cmd[1]] = cptDossiers
+                else:
+                    dico[str(courant)]= {cmd[1] : cptDossiers}
+            else:
+                ptrCourant.append(int(cmd[0]))  # append le fichier dans l'arbre
+           
             cptDossiers += 1
 
-    tailleDossiers = [] # calcul de la taille de chaque dossier
-    for doss in arbre:
-        if tailleDossier(doss) > 100000:
-            guez
+    print("Part 1 :",dossiersAvecMoinsDe(arbre,100_000))
 
-    print("Part 1 :",cpt1)
-
-    cpt2 = 0
-	
-    print("Part 2 :",cpt2)
-
-    if cpt1 != 95437:
-        break
+    tailleALiberer = tailleDossier(arbre)-40_000_000
+    print("Part 2 :",dossierASupprimer(arbre,tailleALiberer))
