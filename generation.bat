8992e6492ec69ad1d1c66d26c3c4cd2cf13c38d2
Gatien ACER <le.magicien.annonyme@gmail.com>
2022-12-09 23:48:02 +0100

    Rework

--- generation.bat
+++ generation.bat
@@ -1,8 +1,7 @@
-for /l %%x in (6, 1, 25) do (
+for /l %%x in (10, 1, 12) do (
    MKDIR day%%x
-   CD day%%x
-   echo. 2>day%%x.py
-   echo. 2>input.txt
-   echo. 2>test.txt
-   CD ..
+   echo. 2>day%%x/day%%x.py
+   echo. 2>day%%x/input.txt
+   echo. 2>day%%x/test.txt
 )
+Pause
