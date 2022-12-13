043b956188e4e5d61bb2f3c0015103f14f94cedd
Gatien ACER <le.magicien.annonyme@gmail.com>
2022-12-13 14:13:38 +0100

    Rework

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 from datetime import datetime
 year = datetime.now().year
 day = datetime.now().day
-print(f"☆ DAY {day} ☆")
+print(f"☆ {year} DAY {day} ☆")
 os.system(f"python {year}/day{day}/day{day}.py")
