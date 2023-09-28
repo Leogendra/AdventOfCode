1d2f989354c79e87e226c2fa9ba0ac11dfdb906d
Gatien <le.magicien.annonyme@gmail.com>
2023-09-28 15:40:47 +0200

    2020 - Day2

---
+++ 2020/day2/day2.py
@@ -0,0 +1,9 @@
+for fichier in ["input"]:
+    print(f"\n*** FICHIER {fichier}.txt ***")
+    input = [line.strip() for line in open(f"2020/day2/{fichier}.txt", "r")]
+
+    print("--- Part One ---")
+
+    print("\n--- Part Two ---")
+    
+    break
