29a12282734de4ad8b746ce3d3f95e8b19574729
Gatien <le.magicien.annonyme@gmail.com>
2023-10-10 22:54:47 +0200

    Updated launcher

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import os
 from datetime import datetime
 year = datetime.now().year
 day = datetime.now().day
+file_path = f"{year}/day{day}/day{day}.py"
+
+while not os.path.exists(file_path):
+    year = input("year: ")
+    day = input("day: ")
+    file_path = f"{year}/day{day}/day{day}.py"
+    
 print(f"☆ {year} DAY {day} ☆")
-os.system(f"python {year}/day{day}/day{day}.py")
+os.system(f"python {file_path}")
