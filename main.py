521983d01a750ba5d3081dbd747015f7721370fd
Gatien <le.magicien.annonyme@gmail.com>
2023-10-21 12:51:22 +0200

    Updated launcher

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import os
+import os, sys
 from datetime import datetime
 year = datetime.now().year
 day = datetime.now().day
 file_path = f"{year}/day{day}/day{day}.py"
 
+args = sys.argv
+parameters = args[1:]
+
+try:
+    year = parameters[0]
+    day = parameters[1]
+    file_path = f"{year}/day{day}/day{day}.py"
+except:
+    pass
+
 while not os.path.exists(file_path):
-    year = input("year: ")
-    day = input("day: ")
+    year, day = input("[year] [day]: ").split()
     file_path = f"{year}/day{day}/day{day}.py"
     
 print(f"☆ {year} DAY {day} ☆")
