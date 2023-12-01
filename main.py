3f85a81dcb0592f41c3a100cc9e327ba194a57f2
Leogendra <le.magicien.annonyme@gmail.com>
2023-12-01 11:08:35 +0100

    2023 - Day1

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 from datetime import datetime
 year = datetime.now().year
 day = datetime.now().day
-print(f"☆ {year} DAY {day} ☆")
+print(f"\n\033[91m☆ {year} DAY {day} ☆\033[0m")
 os.system(f"python {year}/day{day}/day{day}.py")
