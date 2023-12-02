383723e26e77bd169c6fe93941b4ec4df8bd131b
Gatien <le.magicien.annonyme@gmail.com>
2023-12-02 15:08:11 +0100

    Modified main to automatically create advent days

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-import os
+import os, sys
 from datetime import datetime
-year = datetime.now().year
-day = datetime.now().day
-print(f"\n\033[91m☆ {year} DAY {day} ☆\033[0m")
-os.system(f"python {year}/day{day}/day{day}.py")
+
+
+def create_file(year, day):
+
+    print(f"Création du répertoire {year}/day{day}...")
+    os.makedirs(f"{year}/day{day}") 
+    
+    template = ""
+    with open("template.txt", "r", encoding="utf-8") as file:
+        template = file.read()
+
+    with open(f"{year}/day{day}/day{day}.py", "w", encoding="utf-8") as file:
+        file.write(template.replace("{real_year}", str(year)).replace("{real_day}", str(day)))
+
+    with open(f"{year}/day{day}/input.txt", "w") as _:
+        pass
+    with open(f"{year}/day{day}/test.txt", "w") as _:
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    year = datetime.now().year
+    day = datetime.now().day
+    file_path = f"{year}/day{day}/day{day}.py"
+
+    args = sys.argv
+    parameters = args[1:]
+
+    try:
+        year = parameters[0]
+        day = parameters[1]
+        file_path = f"{year}/day{day}/day{day}.py"
+    except:
+        pass
+
+    while not os.path.exists(file_path):
+        # check si on est en décembre
+        if (datetime.now().month == 12) and (datetime.now().day < 26):
+            create_file(year, day)
+            break
+
+        year, day = input("[year] [day]: ").split()
+        file_path = f"{year}/day{day}/day{day}.py"
+        
+
+    print(f"\n\033[91m☆ {year} DAY {day} ☆\033[0m")
+    os.system(f"python {year}/day{day}/day{day}.py")
